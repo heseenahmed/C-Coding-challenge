1ec84ecb90acaef6cc3c1c14b854ce205fe04fc0
AlhussienAhmed <heseenahmed123@gmail.com>
2025-11-02 13:05:54 +0200

    2-11

---
+++ Monitor/program.cs
@@ -0,0 +1,53 @@
+﻿using Serilog;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File("health.log", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        double prevCpuMs = 0;
+        DateTime prev = DateTime.UtcNow;
+        var proc = Process.GetCurrentProcess();
+        bool first = true;
+        while (true)
+        {
+            proc.Refresh();
+            var now = DateTime.UtcNow;
+            var cpuMs = proc.TotalProcessorTime.TotalMilliseconds;
+            var elapsedMs = (now - prev).TotalMilliseconds;
+            var cpuPercent = 0.0;
+
+            // ⚡ Skip first iteration because we don’t have a valid baseline yet
+            if (first)
+            {
+                first = false;
+                prevCpuMs = cpuMs;
+                prev = now;
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                continue;
+            }
+
+            if (elapsedMs > 0)
+            {
+                cpuPercent = ((cpuMs - prevCpuMs) / elapsedMs) * 100.0 / Environment.ProcessorCount;
+                prevCpuMs = cpuMs;
+                prev = now;
+            }
+
+            var workingSet = proc.WorkingSet64;
+            var managedHeap = GC.GetTotalMemory(false);
+
+            Log.Information("ts={Timestamp} cpu={CpuPercent:F1}% ws={WorkingSetMB:F2}MB heap={HeapMB:F2}MB",
+                DateTime.UtcNow, cpuPercent,
+                workingSet / (1024.0 * 1024.0),
+                managedHeap / (1024.0 * 1024.0));
+
+            await Task.Delay(TimeSpan.FromSeconds(10));
+        }
+    }
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-# C-Coding-challenge
-Due to the limited time available for the interview assignment, I focused on implementing the required functionality and clean code style rather than building a fully layered clean architecture.
-The core business logic is already placed inside a Service layer, and the controller only handles HTTP input/output  keeping a clear separation of concerns.
-In a real production environment, I would extend this structure into a full Clean Architecture setup, adding dedicated layers for Repositories, CQRS Command/Query Handlers, DTOs, and Validation, to make the solution more scalable and maintainable.
